4fb1531483c3f1460887a8c29814597b85083b4b
Priyanka Danda <priyankadanda47@gmail.com>
2018-07-12 01:05:43 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,7 +3,7 @@ var playlist = {
   };
  
   function updatePlaylist(objectx, artistName, songTitle) {
-  Object.assign({},objectx, {[artistName]:songTitle})
+  Object.assign({objectx}, {[artistName]:songTitle})
   return objectx
 }
 
