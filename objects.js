617c0df5597ec5064f1baa5a1fb163933d578a46
Priyanka Danda <priyankadanda47@gmail.com>
2018-07-12 00:56:42 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,14 @@
+var playlist = {
+  artist1: "song"
+  };
+ 
+  function updatePlaylist(objectx, artistName, songTitle) {
+  Object.assign({},objectx, {[artistName]:songTitle})
+  return objectx
+}
+
+function removeFromPlaylist(objectx, artistName) {
+  
+  delete objectx.artistName;
+  return objectx
+}
